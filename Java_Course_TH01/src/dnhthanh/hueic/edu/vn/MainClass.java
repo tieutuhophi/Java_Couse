403e06c0ae4849871b5b813dec08657047ea16bc
dang thanh <myhoangthanh@yahoo.com>
2018-03-31 18:11:54 +0700

    Ok

--- Java_Course_TH01/src/dnhthanh/hueic/edu/vn/MainClass.java
+++ Java_Course_TH01/src/dnhthanh/hueic/edu/vn/MainClass.java
@@ -3,8 +3,7 @@ package dnhthanh.hueic.edu.vn;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Main Program");
 	}
 
 }
