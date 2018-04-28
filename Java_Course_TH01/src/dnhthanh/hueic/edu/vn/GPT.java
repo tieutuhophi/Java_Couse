afe1d24c64d5112d0b43fadae930f7e671f3fc76
tieutuhophi <37949729+tieutuhophi@users.noreply.github.com>
2018-04-28 16:29:18 +0700

    Update GPT.java

--- Java_Course_TH01/src/dnhthanh/hueic/edu/vn/GPT.java
+++ Java_Course_TH01/src/dnhthanh/hueic/edu/vn/GPT.java
@@ -3,7 +3,27 @@ package dnhthanh.hueic.edu.vn;
 public class GPT {
 
 	public static void thuchien(float a, float b, float c) {
-		//Viết nội dung giải phương trình bậc 2 vào đây
-		
+		float delta = b * b - 4 * a * c;
+		if (a == 0) {
+			if (b == 0) {
+				if (c == 0) {
+					System.out.println("Phương trình vô số nghiệm !");
+				} else {
+					System.out.println("Phương trình vô nghiệm !");
+				}
+			} else {
+				System.out.println("Phương trình có 1 nghiệm x = " + (-c / b));
+			}
+		} 
+		else {
+			if (delta < 0) {
+				System.out.println("Phương trình vô nghiệm !");
+			} else if (delta == 0) {
+				System.out.println("Phương trình có nghiệm kép x1 = x2 = " + (-b / (2 * a)));
+			} else {
+				System.out.println("x1 = " + ((-b + Math.sqrt(delta)) / (2 * a)));
+				System.out.println("x2 = " + ((-b - Math.sqrt(delta)) / (2 * a)));
+			}
+		}
 	}
 }
